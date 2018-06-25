ba604354ece22624fad4465622e85396100aae36
shixinhua@ycode.cn <shixinhua@ycode.cn>
2018-06-25 14:13:57 +0800

    提交验证码

--- wlwm/src/components/register/register.vue
+++ wlwm/src/components/register/register.vue
@@ -1,91 +1,125 @@
 <template>
-    <div id="register">
-      <!--头部-->
-      <div class="container">
-        <div class="head2">
-          <a href=""><img style="height:45px;width:auto;"></a><h1>欢迎注册</h1>
-          <p class="login_tip">
-            我已经注册，马上
-            <a href="#">登录</a>
-          </p>
+  <div id="register">
+    <!--头部-->
+    <div class="container">
+      <div class="head2">
+        <a href=""><img style="height:45px;width:auto;"></a>
+        <h1>欢迎注册</h1>
+        <p class="login_tip">
+          我已经注册，马上
+          <a href="#">登录</a>
+        </p>
+      </div>
+    </div>
+    <!--主体-->
+    <div class="register_con">
+      <div class="register_con_steps">
+        <div class="register_con_step active">
+          <i>1</i>
+          <span>设置登录名</span>
+        </div>
+        <div class="register_con_step">
+          <i>2</i>
+          <span>设置用户信息</span>
+        </div>
+        <div class="register_con_step">
+          <i>3</i>
+          <span>注册成功</span>
         </div>
       </div>
-      <!--主体-->
-      <div class="register_con">
-        <div class="register_con_steps">
-          <div class="register_con_step active">
-            <i>1</i>
-            <span>设置登录名</span>
-          </div>
-          <div class="register_con_step">
-            <i>2</i>
-            <span>设置用户信息</span>
-          </div>
-          <div class="register_con_step">
-            <i>3</i>
-            <span>注册成功</span>
-          </div>
-        </div>
-        <div class="register_con_form">
-          <form id="" action="" method="">
-            <input id="registerId" name="" type="hidden">
-            <div class="reg_form_item">
-              <label class="for_text">手机号码</label>
-              <div class="item">
-                <input id="mobileInput" class="text long" placeholder="请输入手机号码" name="mobile" maxlength="11" type="text">
-                <i class="correct" style="display: none"></i>
-                <p class="tips mobile_info" style="display: none">请输入您的手机号码</p>
-              </div>
+      <div class="register_con_form">
+        <form id="" action="" method="">
+          <input id="registerId" name="" type="hidden">
+          <div class="reg_form_item">
+            <label class="for_text">手机号码</label>
+            <div class="item">
+              <input id="mobileInput" class="text long" placeholder="请输入手机号码" name="mobile" maxlength="11" type="text">
+              <i class="correct" style="display: none"></i>
+              <p class="tips mobile_info" style="display: none">请输入您的手机号码</p>
             </div>
-            <div class="reg_form_item">
-              <label class="for_text">验证码</label>
-              <div class="item">
-                <input id="checkCode" class="text medium" placeholder="请输入图片验证码" name="checkCode" maxlength="4" type="text">
-                <!--图片验证码-->
-                <div class="code"></div>
-                <p class="tips mobile_info" style="display: none">请输入验证码</p>
+          </div>
+          <div class="reg_form_item">
+            <label class="for_text">验证码</label>
+            <div class="item">
+              <input id="checkCode" class="text medium" placeholder="请输入图片验证码" name="checkCode" maxlength="4" type="text">
+              <!--图片验证码-->
+              <div class="pic_code">
+              <identify :identifyCode="identifyCode" style="margin-top:10px;"></identify>
+              <span class="code" @click="refreshCode"></span>
               </div>
+             
+              <p class="tips mobile_info" style="display: none">请输入验证码</p>
             </div>
-            <div class="reg_form_item">
-              <label class="for_text"></label>
-              <div class="item">
-                <div class="agreement_confirm">
-                  <div class="icheckbox_square-green checked">
-                    <input id="readme" class="i-checks" checked="" type="checkbox" style="position: absolute; opacity: 0">
-                    <ins class="iCheck-helper"></ins>
-                  </div>
-                  我已阅读并同意
-                  <a href="#">《商城用户注册协议》</a>
-                </div>
-                <p class="tips mobile_info" style="display: none">请接受服务条款</p>
-              </div>
-            </div>
-          </form>
+          </div>
           <div class="reg_form_item">
             <label class="for_text"></label>
             <div class="item">
-              <button class="sub_btn" type="submit">同意协议并注册</button>
+              <div class="agreement_confirm">
+                <div class="icheckbox_square-green checked">
+                  <input id="readme" class="i-checks" checked="" type="checkbox" style="position: absolute; opacity: 0">
+                  <ins class="iCheck-helper"></ins>
+                </div>
+                我已阅读并同意
+                <a href="#">《商城用户注册协议》</a>
+              </div>
+              <p class="tips mobile_info" style="display: none">请接受服务条款</p>
             </div>
+          </div>
+        </form>
+        <div class="reg_form_item">
+          <label class="for_text"></label>
+          <div class="item">
+            <button class="sub_btn" type="submit">同意协议并注册</button>
           </div>
         </div>
       </div>
-      <!--页脚-->
-      <div class="container">
-        <p class="login_footer"> Copyright © 2018-2038  万联万名开发平台   苏ICP备17067244</p>
-      </div>
     </div>
+    <!--页脚-->
+    <div class="container">
+      <p class="login_footer"> Copyright © 2018-2038 万联万名开发平台 苏ICP备17067244</p>
+    </div>
+  </div>
 </template>
 
 <style>
-
+  @import url(../../common/css/register);
 </style>
 
 <script>
-    export default {
-        data() {
-            return {
+import SIdentify from '@/components/pictureValidation/identify'
 
-            }
+  export default {
+    data() {
+      return {
+        identifyCodes: "1234567890",
+        identifyCode: ""
+      }
+    },
+    components: {
+      identify:SIdentify
+    },
+    mounted() {
+      this.identifyCode = "";
+      this.makeCode(this.identifyCodes, 4);
+    },
+    methods: {
+      randomNum(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+      },
+      refreshCode() {
+        this.identifyCode = "";
+        this.makeCode(this.identifyCodes, 4);
+      },
+      makeCode(o, l) {
+        for (let i = 0; i < l; i++) {
+          this.identifyCode += this.identifyCodes[
+            this.randomNum(0, this.identifyCodes.length)
+          ];
         }
+        console.log(this.identifyCode);
+      }
     }
+  }
 </script>
+
+
