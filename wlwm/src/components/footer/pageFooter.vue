3eb88d923a85e1687b200272f2b80c4a97b01507
cuijiaojiao <cuijiaojiao@grandage.cn>
2018-06-27 20:57:50 +0800

    本地代码提交

--- wlwm/src/components/footer/pageFooter.vue
+++ wlwm/src/components/footer/pageFooter.vue
@@ -1,92 +1,81 @@
 <template>
-<div class="bottomF">
-    <div class="wp">
-        <div class="row">
-            <div class="col-6"><i class="icons icon_bt_01"></i>正品保障
-                <div>海外直采货源透明</div>
+    <div class="bottomF">
+        <div class="wp">
+            <div class="row">
+                <div class="col-6"><i class="icons icon_bt_01"></i>
+                    正品保障
+                    <div>海外直采货源透明</div>
+                </div>
+                <div class="col-6"><i class="icons icon_bt_02"></i>
+                    闪电发货
+                    <div>电子清关保税仓直发</div>
+                </div>
+                <div class="col-6"><i class="icons icon_bt_03"></i>
+                    政府保障
+                    <div>国检监督海关备案</div>
+                </div>
+                <div class="col-6"><i class="icons icon_bt_04"></i>
+                    无忧售后
+                    <div>国内售后七天包退</div>
+                </div>
             </div>
-            <div class="col-6"><i class="icons icon_bt_02"></i>闪电发货
-                <div>电子清关保税仓直发</div>
-            </div>
-            <div class="col-6"><i class="icons icon_bt_03"></i>政府保障
-                <div>国检监督海关备案</div>
-            </div>
-            <div class="col-6"><i class="icons icon_bt_04"></i>无忧售后
-                <div>国内售后七天包退</div>
-            </div>
-        </div>
 
-        <div class="service  clearfix">
-            <dl class="svc_box fl">
-                <dt>购物指南</dt>
+            <div class="service  clearfix">
+                <dl class="svc_box fl">
+                    <dt>购物指南</dt>
                     <dd>
-                        <div><a href="help/118"
-                            target="_blank">用户注册</a></div>
-                        <div><a href="help/116"
-                            target="_blank">账户安全</a></div>
-                        <div><a href="help/117"
-                            target="_blank">联系客服</a></div>
-                        <div><a href="help/115"
-                            target="_blank">购物流程</a></div>
+                        <div><a href="help/118" target="_blank">用户注册</a></div>
+                        <div><a href="help/116" target="_blank">账户安全</a></div>
+                        <div><a href="help/117" target="_blank">联系客服</a></div>
+                        <div><a href="help/115" target="_blank">购物流程</a></div>
                     </dd>
                 </dl>
-            <dl class="svc_box fl">
-                <dt>帮助中心</dt>
+                <dl class="svc_box fl">
+                    <dt>帮助中心</dt>
                     <dd>
-                        <div><a href="help/146"
-                            target="_blank">函百平台承诺</a></div>
+                        <div><a href="help/146" target="_blank">函百平台承诺</a></div>
                     </dd>
-            </dl>
+                </dl>
 
-            <dl class="svc_box fl">
-                <dt>配送方式</dt>
+                <dl class="svc_box fl">
+                    <dt>配送方式</dt>
                     <dd>
-                        <div><a href="help/119"
-                            target="_blank">函百快递</a></div>
-                        <div><a href="help/121"
-                            target="_blank">上门自提</a></div>
-                        <div><a href="help/120"
-                            target="_blank">物流费用收费标准</a></div>
-                        <div><a href="help/122"
-                            target="_blank">配送服务说明</a></div>
-                        </dd>
-            </dl>
-
-            <dl class="svc_box fl">
-                <dt>支付方式</dt>
-                    <dd>
-                        <div><a href="help/125"
-                            target="_blank">在线支付</a></div>
+                        <div><a href="help/119" target="_blank">函百快递</a></div>
+                        <div><a href="help/121" target="_blank">上门自提</a></div>
+                        <div><a href="help/120" target="_blank">物流费用收费标准</a></div>
+                        <div><a href="help/122" target="_blank">配送服务说明</a></div>
                     </dd>
-            </dl>
+                </dl>
 
-            <dl class="svc_box fl">
-                <dt>售后服务</dt>
+                <dl class="svc_box fl">
+                    <dt>支付方式</dt>
                     <dd>
-                        <div><a href="help/123"
-                            target="_blank">售后政策</a></div>
-                        <div><a href="help/128"
-                            target="_blank">价格保护</a></div>
-                        <div><a href="help/129"
-                            target="_blank">退单说明</a></div>
-                        <div><a href="help/130"
-                            target="_blank">取消订单</a></div>
+                        <div><a href="help/125" target="_blank">在线支付</a></div>
                     </dd>
-            </dl>
+                </dl>
 
-            <div class="qrCode">
-                <div class="codeImg"></div>
-                万联万民微信公众号
-            </div>
-        </div><!--/service-->
-    </div><!--/wp-->
+                <dl class="svc_box fl">
+                    <dt>售后服务</dt>
+                    <dd>
+                        <div><a href="help/123" target="_blank">售后政策</a></div>
+                        <div><a href="help/128" target="_blank">价格保护</a></div>
+                        <div><a href="help/129" target="_blank">退单说明</a></div>
+                        <div><a href="help/130" target="_blank">取消订单</a></div>
+                    </dd>
+                </dl>
 
-    <!--页脚-->
-    <div class="container">
-      <p class="login_footer"> Copyright © 2018-2038 万联万名交易平台 苏ICP备xxxxxx</p>
+                <div class="qrCode">
+                    <div class="codeImg"></div>
+                    万联万民微信公众号
+                </div>
+            </div><!--/service-->
+        </div><!--/wp-->
+
+        <!--页脚-->
+        <div class="container">
+          <p class="login_footer"> Copyright © 2018-2038 万联万名交易平台 苏ICP备xxxxxx</p>
+        </div>
     </div>
-
-</div>
 </template>
 
 <style>
